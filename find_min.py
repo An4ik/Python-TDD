75e143b40549c5e27ef706ae96ec8ede21789a17
Sultan <sultansailaubaev1998@gmail.com>
2018-10-11 23:24:08 +0600

    find_min implemented realization, all tests are passing

--- find_min.py
+++ find_min.py
@@ -5,11 +5,18 @@ def get_min(a, b):
         return min number among a and b
     """
 
+    if a < b:
+        return a
+    else:
+        return b
+
 
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
+
+    raise TypeError("No argument error")
 
 
 def get_min_with_one_argument(x):
@@ -17,11 +24,21 @@ def get_min_with_one_argument(x):
         return that value
     """
 
+    return x
+
 
 def get_min_with_many_arguments(*args):
     """
         return smallest number among args
     """
+
+    minNumber = float('inf')
+
+    for arg in args:
+        if arg < minNumber:
+            minNumber = arg
+
+    return minNumber
 
 
 def get_min_with_one_or_more_arguments(first, *args):
@@ -29,11 +46,27 @@ def get_min_with_one_or_more_arguments(first, *args):
         return smallest number among first + args
     """
 
+    minNumber = float('inf')
+
+    for arg in (first, *args):
+        if arg < minNumber:
+            minNumber = arg
+
+    return minNumber
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
+
+    minNumber = float('Inf')
+
+    for arg in args:
+        if arg < minNumber and (low < arg and arg < high):
+            minNumber = arg
+
+    return minNumber
+
 
 
 def make_min(*, low, high):
@@ -45,6 +78,11 @@ def make_min(*, low, high):
     """
 
     def inner(first, *args):
-        pass
+        minNumber = float('inf')
+
+        for arg in (first, *args):
+            if arg < minNumber and (arg > low and arg < high):
+                minNumber = arg
+        return minNumber
 
     return inner
