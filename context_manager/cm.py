d9ac4854dedfda3436c7f9cdd8dbf1b529b39966
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2018-10-26 03:44:44 +0600

    more tests

--- context_manager/cm.py
+++ context_manager/cm.py
@@ -42,24 +42,30 @@ def open_file(file, mode):
     f = open(file, mode)
     yield f
     f.close()
+        
 
 def check_open_file(filename):
     with open_file(filename, 'w') as f:
         f.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
-    print(f.closed)
+    if(f.closed):
+        return True
+    else:
+        return False
+        
 
 
 #### CD Example ####
 def change_dir_without_context_manager(filename1, filename2):
     cwd = os.getcwd()
     os.chdir(filename1)
-    print(os.listdir())
+    a = os.listdir()
     os.chdir(cwd)
     
     cwd = os.getcwd()
     os.chdir(filename2)
-    print(os.listdir())
+    b = os.listdir()
     os.chdir(cwd)
+    return a + b
 
 
         
@@ -81,6 +87,8 @@ def try_change_dir(filename):
 simple_open_and_write_without_context_manager('first.txt')
 simple_open_and_write_with_context_manager('second.txt')
 check_open_file('third')
-change_dir_without_context_manager('file_one','file_two')
+print(change_dir_without_context_manager('file_one','file_two'))
+print(change_dir_without_context_manager('file_one','file_one'))
+print(change_dir_without_context_manager('file_two','file_two'))
 try_change_dir('file_one')
 try_change_dir('file_two')
--- context_manager/tests/context_manager_test.py
+++ context_manager/tests/context_manager_test.py
@@ -11,3 +11,19 @@ class ContextManagerTest(TestCase):
         result = f.simple_open_and_write_with_context_manager('second.txt')
         self.assertEqual(result, True)
 
+    def test_check_open_file(self):
+        result = f.check_open_file('third.txt')
+        self.assertEqual(result, True)
+
+    def test_change_dir_without(self):
+        result = f.change_dir_without_context_manager('file_one','file_two')
+        self.assertEqual(result, ['love.txt', 'context.txt', 'I.txt', 'manager.txt', 'love.txt', 'and.txt', 'I.txt', 'beshbarmak.txt'])
+
+    def test_change_dir_without1(self):
+        result = f.change_dir_without_context_manager('file_one', 'file_one')
+        self.assertEqual(result, ['love.txt', 'context.txt', 'I.txt', 'manager.txt', 'love.txt', 'context.txt', 'I.txt', 'manager.txt'])
+
+    def test_change_dir_without2(self):
+        result = f.change_dir_without_context_manager('file_two', 'file_two')
+        self.assertEqual(result, ['love.txt', 'and.txt', 'I.txt', 'beshbarmak.txt', 'love.txt', 'and.txt', 'I.txt', 'beshbarmak.txt'])
+
