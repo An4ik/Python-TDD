b3082d02cf2fbcec6506e09a410ab2d830cbb927
iamroll <rollwho@gmail.com>
2018-10-26 10:07:56 +0600

    add description

--- fibonacci/answers/brute_force_fibonacci.py
+++ fibonacci/answers/brute_force_fibonacci.py
@@ -1,6 +1,20 @@
+"""
+Fibonacci number.
+Fibonacci number a series of numbers in which each number ( Fibonacci number ) is the sum of the two previous numbers.
+The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+"""
+
+
 class BruteForceFibonacci(object):
+    """
+    In this RecursiveFibonacci class, we realized recursive implementation.
+    """
 
     def __init__(self):
+        """
+        __init__ method can be called as constructor on other languages. Interpretator called when you create an
+        instance.
+        """
         self.cache = {}
 
     def recursive_implementation(self, n):
@@ -8,6 +22,8 @@ class BruteForceFibonacci(object):
         :param n: n-th number of fibonacci numbers
         :rtype: int
         :return: fibonacci number
+
+        Recursion means calling functions directly from itself.
         """
 
         if n < 0:
