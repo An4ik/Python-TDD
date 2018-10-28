672099de6308db0cffe52d3dc4c2eb8b58ba1eb3
An4ik <akhmetov.anuar09@gmail.com>
2018-10-28 19:36:39 +0600

    fixed tests for brute force fib

--- fibonacci/brute_force.py
+++ fibonacci/brute_force.py
@@ -16,3 +16,11 @@ class Fibonacci:
         :rtype: int
         :return: fibonacci number
         """
+
+        if n < 0:
+            raise TypeError('Please, enter positive number.')
+
+        if n <= 2:
+            return 1 if n == 2 else 0
+
+        return self.get_number(n - 2) + self.get_number(n - 1)
