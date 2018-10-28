96b6b288a254ae38619c88efcc14600ae40aebcd
An4ik <akhmetov.anuar09@gmail.com>
2018-10-28 12:22:42 +0600

    fixed up recursively method

--- fibonacci/answers/brute_force.py
+++ fibonacci/answers/brute_force.py
@@ -3,39 +3,21 @@ Fibanacci implementation using recursive method.
 """
 
 
-class BruteForceFibonacci(object):
-    """
-    In this RecursiveFibonacci class, we realized recursive implementation.
-    """
-
-    def __init__(self):
-        """
-        __init__ method can be called as constructor on other languages. Interpretator called when you create an
-        instance.
-        """
-        self.cache = {}
+class Fibonacci:
 
     def get_number(self, n):
         """
         Returns n-th number of fibonacci numbers.
 
+        Returns 1 or 0 for 'n' less than 3, otherwise recursively calls itself with appropriate arguments
+        (sum of two previous numbers).
+
         :param n: n-th number of fibonacci numbers
         :rtype: int
         :return: fibonacci number
-
-        Recursion means calling functions directly from itself.
         """
 
-        if n < 0:
-            raise TypeError('please enter positive numbers')
+        if n <= 2:
+            return 1 if n == 2 else 0
 
-        if n in self.cache:
-            ans = self.cache[n]
-        elif n <= 2:
-            ans = 1
-            self.cache[n] = ans
-        else:
-            ans = self.get_number(n - 2) + self.get_number(n - 1)
-            self.cache[n] = ans
-
-        return ans
+        return self.get_number(n - 2) + self.get_number(n - 1)
