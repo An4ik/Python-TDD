17c904f036eaa2ded26979d5733217a3ac0b1316
An4ik <akhmetov.anuar09@gmail.com>
2018-10-28 21:36:12 +0600

    changed the first decorator

---
+++ decorator/tests/test_stringify.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from decorators.stringify import add, multiply
+
+
+class StringifyTestCase(TestCase):
+    def test_add_returns_string(self):
+        result = add(5, 6)
+        self.assertTrue(isinstance(result, str))
+
+    def test_add_does_right_calculation(self):
+        result = add(5, 6)
+        self.assertEqual(int(result), 11)
+
+    def test_multiply_returns_string(self):
+        result = multiply(5, 6)
+        self.assertTrue(isinstance(result, str))
+
+    def test_multiply_does_right_calculation(self):
+        result = multiply(5, 6)
+        self.assertEqual(int(result), 30)
