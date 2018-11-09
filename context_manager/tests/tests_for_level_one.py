2a51fa107197959421c31db2f5945dd29d3dccd0
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2018-11-09 16:04:38 +0600

    fix level one and first rst

--- context_manager/tests/tests_for_level_one.py
+++ context_manager/tests/tests_for_level_one.py
@@ -1,27 +1,32 @@
 from unittest import TestCase
-import write_into_file as f
+from write_into_file import (
+    write_without_context_manager,
+    write_with_context_manager,
+    check_open_file,
+    read_file
+)
 
 class LevelOneTest(TestCase):
     def test_write_without(self):
-        result = f.write_without_context_manager('first.txt')
+        result = write_without_context_manager('first.txt')
         self.assertEqual(result, True)
 
     def test_write_with(self):
-        result = f.write_with_context_manager('second.txt')
+        result = write_with_context_manager('second.txt')
         self.assertEqual(result, True)
 
     def test_check_open_file(self):
-        result = f.check_open_file('third.txt')
+        result = check_open_file('third.txt')
         self.assertEqual(result, True)
 
     def test_write_without1(self):
-        result = f.read_file('first.txt')
+        result = read_file('first.txt')
         self.assertEqual(result, 'simple open and write done!')
 
     def test_write_with1(self):
-        result = f.read_file('second.txt')
+        result = read_file('second.txt')
         self.assertEqual(result, 'write with context manager done!')
 
     def test_check_open_file1(self):
-        result = f.read_file('third.txt')
+        result = read_file('third.txt')
         self.assertEqual(result, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
